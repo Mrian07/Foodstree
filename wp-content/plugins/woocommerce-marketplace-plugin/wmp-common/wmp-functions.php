c09fbf6053743a7fe74480f76f27fc97244e6e4b
robiul <robiul@ajency.in>
2015-01-06 09:53:17 +0530

    fixed issue#19

--- wp-content/plugins/woocommerce-marketplace-plugin/wmp-common/wmp-functions.php
+++ wp-content/plugins/woocommerce-marketplace-plugin/wmp-common/wmp-functions.php
@@ -1,5 +1,39 @@
 <?php
 
+
+//Override woocommerce template in plugin
+function myplugin_plugin_path() {
+  // gets the absolute path to this plugin directory
+  return untrailingslashit( WMP_DIR );
+}
+add_filter( 'woocommerce_locate_template', 'myplugin_woocommerce_locate_template', 10, 3 );
+function myplugin_woocommerce_locate_template( $template, $template_name, $template_path) {
+  global$woocommerce;
+  $_template= $template;
+  if( ! $template_path) $template_path= $woocommerce->template_url;
+  $plugin_path= myplugin_plugin_path() . '/frontend-templates/';
+  // Look within passed path within the theme - this is priority
+  $template= locate_template(
+    array(
+      $template_path. $template_name,
+      $template_name
+    )
+  );
+  // Modification: Get the template from this plugin, if it exists
+  if( ! $template && file_exists( $plugin_path. $template_name) )
+   $template= $plugin_path. $template_name;
+   
+  // Use default template
+  if( ! $template)
+    $template= $_template;
+  // Return what we found
+  return $template;
+}
+
+
+
+
+
 //count product by seller_id
 function count_seller_products( $seller_id ) {
 global $wpdb;
@@ -108,6 +142,22 @@ function wmp_columns_content_only_sellers($column_name, $post_ID) {
         $seller_id = $post_tmp->post_author;
         echo get_seller_name($seller_id);
     }
+}
+
+
+
+
+
+function wmp_get_suborders($orderid){
+  $args = array(
+  'post_parent' => $orderid,
+  'post_type'   => 'shop_order', 
+  'posts_per_page' => -1,
+  'post_status' => 'any' );
+
+$suborders = get_children( $args, OBJECT );
+
+return $suborders;
 }
 
 
